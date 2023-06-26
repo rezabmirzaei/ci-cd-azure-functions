24511ab50d865e87e292066af6fc537183f3ed85
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-06-26 15:02:35 +0200

    Testing workflow

--- DemoHttpTrigger/index.js
+++ DemoHttpTrigger/index.js
@@ -1,18 +1,10 @@
 module.exports = async function (context, req) {
 
-    const currentDate = new Date();
-    const date = (`0 ${currentDate.getDate()}`).slice(-2);
-    const month = (`0 ${currentDate.getMonth() + 1}`).slice(-2);
-    const year = currentDate.getFullYear();
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const now = (`${year}-${month}-${date} ${hours}:${minutes}`);
-
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function was developed in Visual Studio Code."
-        : (`This HTTP triggered function was developed in Visual Studio Code 
-                and automatically deployed using GitHub actions at ${now}`);
+        : "This HTTP triggered function was developed in Visual Studio Code \
+                and automatically deployed using GitHub actions on June 26th @13:00";
 
     context.res = {
         body: responseMessage
