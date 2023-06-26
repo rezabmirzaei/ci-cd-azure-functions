10bf78f074a4800fe1b66ba0c3b47462f00720d7
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-06-26 14:48:56 +0200

    Testing workflow

--- DemoHttpTrigger/index.js
+++ DemoHttpTrigger/index.js
@@ -1,13 +1,12 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function was developed in Visual Studio Code."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "This HTTP triggered function was developed in Visual Studio Code and automatically deployed using GitHub actions.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+
 }
